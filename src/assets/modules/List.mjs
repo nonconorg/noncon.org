1e193bbdf69548b790c144597e3305b3dd00a207
jascha <jascha@jaeh.at>
2020-02-24 04:34:56 +0100

    simplify list content

--- src/assets/modules/List.mjs
+++ src/assets/modules/List.mjs
@@ -1,9 +1,7 @@
 export const View = items =>
   ul(
     { class: 'List' },
-    items.map(i => [
-      li([Img({ src: i.img, height: '90' }), div([h4(Link({ to: i.to }, i.title)), p(i.text)])]),
-    ]),
+    items.map(i => [li([Img({ src: i.img, height: '90' }), Link({ to: i.to }, i.title)])]),
   )
 
 export const style = {
@@ -12,33 +10,30 @@ export const style = {
   li: {
     clear: 'both',
     display: 'inline-block',
-    margin: '0.5em 0',
-    width: '100%',
+    margin: '0.5em 0 5em',
+    width: '48%',
+    margin: '0 0.99% 5em',
     textAlign: 'center',
-
-    h4: {
-      lineHeight: 1,
-      padding: 0,
-    },
   },
 
   img: {
     width: 'auto',
-    margin: '0 0 1em',
+    display: 'block',
+    margin: '0 auto 1em',
   },
 
   '@media screen and (min-width: 600px)': {
     li: {
       clear: 'none',
-      width: '49%',
-      margin: '0 0.99%',
+      width: '30%',
+      margin: '0 1.49% 5em',
     },
   },
 
   '@media screen and (min-width: 850px)': {
     li: {
-      width: '30%',
-      margin: '0 1.49%',
+      width: '23%',
+      margin: '0 0.99% 5em',
     },
   },
 }
