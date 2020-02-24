5445100a29d98e7bff1d53fdd147b1c6880e458e
jascha <jascha@jaeh.at>
2020-02-24 06:00:26 +0100

    Footer: remove parallele link duplicate

--- src/assets/modules/Footer.mjs
+++ src/assets/modules/Footer.mjs
@@ -28,7 +28,7 @@ export const View = state => {
       ]),
 
       div({ class: 'Credits' }, [
-        p([ 'created by the ', Link({ to: 'https://parallele.at' }, 'parallele polis dao')]),
+        p('created by the parallele polis dao'),
         p([
           'made with a few bits of ',
           Link({ to: 'https://github.com/magic/core', target: '_blank', rel: 'noopener' }, 'magic'),
