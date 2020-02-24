10c42ce61869adba3709c1ffaa7c794fa20a7f45
jascha <jascha@jaeh.at>
2020-02-24 19:27:09 +0100

    HeroImage: black background on light theme

--- src/assets/modules/HeroImage.mjs
+++ src/assets/modules/HeroImage.mjs
@@ -1,14 +1,15 @@
 export const View = () =>
   p({ class: 'HeroImage' }, Img({ height: 300, src: '/img/noncon2020-banner.jpg' }))
 
-export const style = {
+export const style = vars => ({
   width: '100%',
   textAlign: 'center',
   padding: '1.5em 0 0',
+  backgroundColor: vars.background.dark,
 
   img: {
     width: 'auto',
     height: 'auto',
     maxWidth: '70vw',
   },
-}
+})
