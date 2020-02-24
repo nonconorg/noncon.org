a7af59d43b11923528780b02c28252e991ccd46b
jascha <jascha@jaeh.at>
2020-02-24 05:48:53 +0100

    simplify hero

--- src/assets/modules/Hero.mjs
+++ src/assets/modules/Hero.mjs
@@ -1,102 +1,15 @@
-export const View = ({ hero, ...state }) => {
-  const title = Array.isArray(hero.title) ? hero.title : [hero.title]
+export const View = ({ hero, ...state }) =>
+  p({ class: 'Hero' }, Img({ height: 300, src: '/img/noncon2020-banner.png' }))
 
-  let { content } = hero
-  if (content && !Array.isArray(content)) {
-    content = [content]
-  }
-
-  return div({ id: 'hero', class: 'Hero' }, [
-    div({ class: 'LogoWrapper' }, [
-      Img({ height: 300, src: '/img/noncon2020-banner.png' }),
-      hero.title && h1([span(title[0]), title[1]]),
-      hero.description && p(hero.description),
-    ]),
-
-    content &&
-      div(
-        { class: 'content' },
-        content.map(a => p(a)),
-      ),
-
-    hero.menu &&
-      Menu({
-        items: hero.menu,
-        state,
-      }),
-  ])
-}
 
 export const style = vars => ({
-  backgroundColor: vars.background.dark,
-  display: 'inline-block',
-  margin: '5vh auto 0',
-  position: 'relative',
-  maxWidth: 'inherit',
   width: '100%',
+  textAlign: 'center',
+  padding: '1.5em 0 0',
 
   img: {
     width: 'auto',
-  },
-
-  '.content': {
-    clear: 'both',
-    padding: '1.5em 0',
-  },
-
-  '.LogoWrapper': {
-    margin: '0 auto',
-    position: 'relative',
-    minWidth: '250px',
-    maxWidth: 'inherit',
-    width: '70vw',
-
-    '.Logo': {
-      float: 'none',
-    },
-
-    h1: {
-      color: vars.text.dark,
-      fontSize: '2em',
-      padding: '0',
-      textAlign: 'center',
-    },
-  },
-
-  '.Menu': {
-    float: 'none',
-    textAlign: 'center',
-    padding: 0,
-    width: '100%',
-    clear: 'both',
-
-    ul: {
-      width: 'auto',
-    },
-
-    a: {
-      color: vars.text.dark,
-
-      '&:hover': {
-        color: vars.neutral,
-      },
-    },
-  },
-
-  p: {
-    color: vars.text.dark,
-    fontSize: '1.1em',
-    textAlign: 'center',
-  },
-
-  [`@media screen and (min-width: ${vars.widths.tablet})`]: {
-    '.LogoWrapper': {
-      '.Logo': {
-        svg: {
-          float: 'left',
-          width: '35%',
-        },
-      },
-    },
+    height: 'auto',
+    maxWidth: '70vw',
   },
 })
