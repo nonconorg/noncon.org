4c2c9ba4e74a03776d3ef974b5688087ab531569
jascha <jascha@jaeh.at>
2020-02-24 01:15:11 +0100

    pages/privacy: add first draft of contents

--- src/pages/privacy.mjs
+++ src/pages/privacy.mjs
@@ -3,4 +3,31 @@ export const state = {
   description: 'noncon.org privacy',
 }
 
-export const View = state => [div([h2('Privacy Notice'), h2('stay tuned.')])]
+export const View = state => [
+  div([
+    h2('Privacy Notice'),
+
+    p('This page does not:'),
+    p([
+      'Collect personal data, load tracking scripts,',
+      ' or use any other means of spying on its users',
+    ]),
+
+    p([
+      'Since this page and the connecting databases were setup in about 12 hours,',
+      ' so we used ',
+      Link({ to: 'https://www.typeform.com/' }, 'typeform'),
+      ' to implement the form.',
+      ' For now, we save all submitted forms in a google document.',
+      ' This is expected to change very soon.',
+    ]),
+
+    p('We will in no way try to correlate form inputs with ticket purchases'),
+
+    p([
+      'All purchasers of tickets will receive an non-personalized ntf token.',
+      ' This token will not be required for entry, ! just come !,',
+      ' but will unlock some awesome goodies instead.',
+    ]),
+  ]),
+]
