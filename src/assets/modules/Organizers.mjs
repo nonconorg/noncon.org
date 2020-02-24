d1ac246bf202ade0aac0085c1bdcdc7223bf843f
jascha <jascha@jaeh.at>
2020-02-24 23:08:51 +0100

    Organizers: only three, so li min width is 33%

--- src/assets/modules/Organizers.mjs
+++ src/assets/modules/Organizers.mjs
@@ -1,20 +1,32 @@
-export const View = () => [
-  h2('Organizers'),
-  List([
-    {
-      img: '/img/organizers/parallele.jpg',
-      to: 'https://parallele.at',
-      title: 'parallele polis',
+export const View = () =>
+  div({ class: 'Organizers' }, [
+    h2('Organizers'),
+    List([
+      {
+        img: '/img/organizers/parallele.jpg',
+        to: 'https://parallele.at',
+        title: 'parallele polis',
+      },
+      {
+        img: '/img/organizers/bwb.png',
+        to: 'https://bwb.is',
+        title: 'bwb.is',
+      },
+      {
+        img: '/img/organizers/riat.jpg',
+        to: 'https://riat.at',
+        title: 'riat.at',
+      },
+    ]),
+  ])
+
+export const style = {
+  '@media screen and (min-width: 850px)': {
+    '.List': {
+      li: {
+        width: '30%',
+        margin: '0 1.49% 5em',
+      },
     },
-    {
-      img: '/img/organizers/bwb.png',
-      to: 'https://bwb.is',
-      title: 'bwb.is',
-    },
-    {
-      img: '/img/organizers/riat.jpg',
-      to: 'https://riat.at',
-      title: 'riat.at',
-    },
-  ]),
-]
+  },
+}
