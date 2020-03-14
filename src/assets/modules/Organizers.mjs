43fc5c8d90802bb9592e3066ab73ebf305560e79
jascha <jascha@jaeh.at>
2020-03-14 06:59:28 +0100

    4 organizers now

--- src/assets/modules/Organizers.mjs
+++ src/assets/modules/Organizers.mjs
@@ -1,21 +1,4 @@
 export const View = props => List({ class: 'Organizers', ...props })
 
 export const style = {
-  li: {
-    display: 'block',
-    width: '80%',
-    margin: '0 auto 5em',
-  },
-
-  '@media screen and (min-width: 850px)': {
-    li: {
-      float: 'left',
-      width: '50%',
-      margin: '0 1.49% 5em',
-
-      '&:first-child, &:last-child': {
-        width: '20%',
-      },
-    },
-  },
 }
