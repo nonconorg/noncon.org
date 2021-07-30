6e27b52f8c764b6b19b2a9eddd55b0474819b4ff
jaeh <jascha@jaeh.at>
2021-07-30 21:24:25 +0200

    update test to remove page, md is not testable yet

--- test/spec.mjs
+++ test/spec.mjs
@@ -1,10 +1,8 @@
 import { is } from '@magic/test'
 
 import * as app from '../src/app.mjs'
-import * as page from '../src/pages/index.mjs'
 
 export default [
   { fn: app, expect: is.object },
   { fn: app.state, expect: is.object },
-  { fn: () => page.View, expect: is.fn },
 ]
